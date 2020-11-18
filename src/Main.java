c5f82eeecc8cec0ae44fc4f3d5a094557dc8d6c1
Alexey Andreev <dark-cross@rambler.ru>
2020-11-18 23:53:50 +0300

    'Miles'

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-    int amount = 4999;
-    int part = 20;
-    int bonus = amount / part;
+        int amount = 4999;
+        int part = 20;
+        int bonus = amount / part;
         System.out.println(bonus);
 
     }
